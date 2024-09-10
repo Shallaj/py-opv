abb753fd0157144cb2f14ec8d17b4c642b091467
Shallaj <Shahin.hallaj@gmail.com>
2024-09-10 16:39:18 -0700

    v0.1.0.7

--- pyopv/opvdicom/__init__.py
+++ pyopv/opvdicom/__init__.py
@@ -29,17 +29,13 @@ class OPVDicom:
             # Check if each tag in nema_opv_dicom is present in the DICOM dataset
             for _, row in self.nema_opv_dicom.iterrows():
                 try:
-                    # Convert tag to a format that can be used to access elements directly
-                    tag_str = row['tag'].strip('()')
-                    tag_group, tag_element = tag_str.split(',')
+                    # Convert self.ds to a string
+                    ds_str = str(self.ds)
+                    tag = row['tag']
 
-                    # Ensure tag is stripped of any spaces and convert to integer
-                    tag_tuple = (int(tag_group.strip(), 16), int(tag_element.strip(), 16))
-
-                    # Access the tag in the DICOM dataset
-                    if tag_tuple not in self.ds:
-                        # Append the missing tag row to the missing_tags DataFrame
-                        missing_tags = pd.concat([missing_tags, pd.DataFrame([row])], ignore_index=True)
+                    # Check if the tag is present in the DICOM dataset
+                    if tag not in ds_str:
+                        missing_tags = missing_tags.append(row, ignore_index=True)
 
                 except ValueError as ve:
                     print(f"Tag parsing error for {row['tag']}: {ve}")
