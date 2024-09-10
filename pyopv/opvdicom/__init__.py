4a01217e4574f0234026dc1feb826ba858d424f2
Shallaj <Shahin.hallaj@gmail.com>
2024-09-10 16:10:51 -0700

    v.0.1.0.5

--- pyopv/opvdicom/__init__.py
+++ pyopv/opvdicom/__init__.py
@@ -23,29 +23,19 @@ class OPVDicom:
             print(f'SOP Class UID is incorrect in {self.filename}: {self.ds.SOPClassUID}')
             print(f'Expected SOP Class UID: 1.2.840.10008.5.1.4.1.1.80.1')
 
+        # Store the complete ds as a string in a variable
+        ds_str = str(self.ds)
+        
         # Initialize an empty DataFrame to store missing tags
         missing_tags = pd.DataFrame(columns=self.nema_opv_dicom.columns)
-
-        # Check if each tag in nema_opv_dicom is present in the DICOM dataset
+    
+        # Check if each tag in nema_opv_dicom is present in ds_str
         for _, row in self.nema_opv_dicom.iterrows():
-            try:
-                # Convert tag to a format that can be used to access elements directly
-                tag_tuple = row['tag']
-                # Access the tag in the DICOM dataset
-                if tag_tuple not in self.ds:
-                    # Append the missing tag row to the missing_tags DataFrame
-                    missing_tags = pd.concat([missing_tags, pd.DataFrame([row])], ignore_index=True)
-                    
-            except ValueError as ve:
-                print(f"Tag parsing error for {row['tag']}: {ve}")
-            except KeyError:
-                print(f"Tag not found: {row['tag']}")
-            except Exception as e:
-                print(f"Error processing tag {row['tag']}: {e}")
-
+            if row['tag'] not in ds_str:
+                missing_tags = pd.concat([missing_tags, pd.DataFrame([row])], ignore_index=True)
+        
         # Return the count of missing tags and the DataFrame of missing tags
         return len(missing_tags), missing_tags
-
 
 
 
