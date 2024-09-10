86bd79163babb620a797b0217b834c1bfa8fae65
Shallaj <Shahin.hallaj@gmail.com>
2024-09-10 13:39:07 -0700

    sync

--- setup.py
+++ setup.py
@@ -5,12 +5,16 @@ setup(
     version='0.1.0',
     author='Shahin Hallaj',
     author_email='shallaj@health.ucsd.edu',
-    description='This package provides a set of tools for checking OPV DICOM compliance and converting OPV DICOM to csv or json.',
+    description='This package provides a set of tools for checking OPV DICOM compliance and converting OPV DICOM to CSV or JSON.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/Shallaj/py_opv',
     packages=find_packages(),
-    install_requires=open('requirements.txt').read().splitlines(),
+    install_requires=[
+        'pydicom',
+        'numpy',
+        'pandas'
+    ],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
